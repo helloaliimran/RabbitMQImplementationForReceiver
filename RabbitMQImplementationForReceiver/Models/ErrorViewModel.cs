8e167ba155c4c05d3610dc3aa8b024c1db27b6ec
imran.ali@cppa.gov.pk <imran.ali@cppa.gov.pk>
2023-10-16 11:21:02 +0500

    Add project files.

---
+++ RabbitMQImplementationForReceiver/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RabbitMQImplementationForReceiver.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ RabbitMQImplementationForReceiver/Models/RabbitMQAuthMessageConsumer.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.ComponentModel;
+using System.Text;
+
+namespace RabbitMQImplementationForReceiver.Models
+{
+	public class RabbitMQAuthMessageConsumer : BackgroundService
+	{
+		private readonly IConfiguration _configuration;
+		private	readonly IConnection _connection;
+		private readonly IModel _channel;
+		public RabbitMQAuthMessageConsumer(IConfiguration configuration)
+        {
+				_configuration = configuration;
+			var factory = new ConnectionFactory
+			{
+				HostName = "localhost",
+				UserName = "guest",
+				Password = "guest",
+			};
+			_connection=factory.CreateConnection();
+			_channel = _connection.CreateModel();
+			_channel.QueueDeclare(_configuration.GetValue<string>("QueueName:RegisterUserQueue"), false, false, false, null);
+
+		}
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			stoppingToken.ThrowIfCancellationRequested();
+
+			var consumer = new EventingBasicConsumer(_channel);
+			consumer.Received += (ch, ea) =>
+			{
+				var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+				String email = JsonConvert.DeserializeObject<string>(content);
+				HandleMessage(email).GetAwaiter().GetResult();
+				_channel.BasicAck(ea.DeliveryTag, false);
+								
+			};
+			_channel.BasicConsume(_configuration.GetValue<string>("QueueName:RegisterUserQueue"), false,consumer);
+			return Task.CompletedTask;
+		}
+
+		private async  Task HandleMessage(String email)
+		{
+			Console.WriteLine(email);
+		}
+	}
+}
